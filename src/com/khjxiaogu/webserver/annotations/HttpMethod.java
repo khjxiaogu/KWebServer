d1e86894ffb87dcf338e4a53ce867184acd8575d
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-06-29 18:37:38 +0800

    fix lang keys not included

--- src/com/khjxiaogu/webserver/annotations/HttpMethod.java
+++ src/com/khjxiaogu/webserver/annotations/HttpMethod.java
@@ -26,12 +26,6 @@ import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
 @Target(METHOD)
-@interface HttpMethods {
-	public HttpMethod[] value();
-}
-
-@Retention(RUNTIME)
-@Target(METHOD)
 @Repeatable(HttpMethods.class)
 public @interface HttpMethod {
 	public String value();
--- src/com/khjxiaogu/webserver/annotations/HttpPath.java
+++ src/com/khjxiaogu/webserver/annotations/HttpPath.java
@@ -26,12 +26,6 @@ import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
 @Target(METHOD)
-@interface HttpPaths {
-	public HttpPath[] value();
-}
-
-@Retention(RUNTIME)
-@Target(METHOD)
 @Repeatable(HttpPaths.class)
 public @interface HttpPath {
 	public String value();
